a4dc7f1fe8e5c3d8a47227a9064b5969371a5bdd
Aspirationtocode <aspiretocode@gmail.com>
2018-01-23 23:58:40 +0300

    last server && socket.io fixes

--- scripts/components/InfoPanel/index.js
+++ scripts/components/InfoPanel/index.js
@@ -11,21 +11,25 @@ import UserInfo from '../UserInfo';
 const InfoPanel = props => {
 	const { routes } = props.nav;
 	const currentScreen = routes[routes.length - 1].routeName;
-	if (currentScreen === 'Themes' || currentScreen === 'Questions') {
-		return (
-			<View style={styles.container}>
-				<View style={styles.infoPanelContainer}>
-					<UserInfo />
+	if (props.currentUser.data) {
+		if (currentScreen === 'Themes' || currentScreen === 'Questions') {
+			return (
+				<View style={styles.container}>
+					<View style={styles.infoPanelContainer}>
+						<UserInfo />
+					</View>
+					<Image source={bgImage} resizeMode="cover" style={styles.bg} />
 				</View>
-				<Image source={bgImage} resizeMode="cover" style={styles.bg} />
-			</View>
-		);
+			);
+		}
 	}
+
 	return null;
 };
 
 const mapStateToProps = state => ({
 	nav: state.nav,
+	currentUser: state.currentUser,
 });
 
 export default connect(mapStateToProps)(InfoPanel);
--- scripts/host.js
+++ scripts/host.js
@@ -1,2 +1,3 @@
-const host = 'http://192.168.1.16:4000';
+// const host = 'http://192.168.1.16:4000';
+const host = 'https://youtbe-application.herokuapp.com';
 export default host;
